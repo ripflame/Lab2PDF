6b742d52a4e136d84dbd6063c49e43146b663c56
Gilberto Leon <ripflame@gmail.com>
2025-02-15 10:17:53 -0600

    Add .gitignore file

--- README.md
+++
@@ -1,3 +0,0 @@
-# Lab2PDF
-
-Simple app to help transcribe lab results into a formated PDF file
--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  generatePDF: (data) => ipcRenderer.send('generate-pdf', data),
-  onPDFGenerated: (callback) => ipcRenderer.on('pdf-generated', callback),
-});
