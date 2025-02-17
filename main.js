41c430f5486216b18bf2319c0d049de9d484943e
Gilberto Leon <ripflame@gmail.com>
2025-02-17 16:28:25 -0600

    Enhance PDF generation by adding formatting for date, age, phone number, and various health metrics

--- main.js
+++ main.js
@@ -37,18 +37,59 @@ ipcMain.on('generarPDF', async (event, formData) => {
   const topImageContent = fs.readFileSync(topImagePath, 'utf8');
   const bottomImageContent = fs.readFileSync(bottomImagePath, 'utf8');
 
+  function formatNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  function formatPhoneNumber(phoneNumber) {
+    return phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '($1)$2-$3');
+  }
+
   try {
     // Load and read the HTML template
     let htmlContent = fs.readFileSync(templatePath, 'utf8');
 
+    // Format the date to dd/mm/yyyy
+    const [year, month, day] = formData.fecha.split('-');
+    const formattedDate = `${day}/${month}/${year}`;
+
+    // Format the age
+    const formattedAge = formData.edad === '1' ? '1 año' : `${formData.edad} años`;
+
     // Replace placeholders with actual data
-    htmlContent = htmlContent.replace('{{nombreMascota}}', formData.nombreMascota)
+    htmlContent = htmlContent.replace('{{requerido}}', formData.requerido)
+                             .replace('{{fecha}}', formattedDate)
+                             .replace('{{nombreMascota}}', formData.nombreMascota)
                              .replace('{{especie}}', formData.especie)
-                             .replace('{{raza}}', formData.raza);
+                             .replace('{{raza}}', formData.raza)
+                             .replace('{{nombrePropietario}}', formData.nombrePropietario)
+                             .replace('{{edad}}', formattedAge)
+                             .replace('{{sexo}}', formData.sexo)
+                             .replace('{{telefono}}', formatPhoneNumber(formData.telefono))
+                             .replace('{{eritrocitos}}', formatNumber(formData.eritrocitos))
+                             .replace('{{hemoglobina}}', formatNumber(formData.hemoglobina))
+                             .replace('{{hematocrito}}', formatNumber(formData.hematocrito))
+                             .replace('{{vgm}}', formatNumber(formData.volumenGlobularMedio))
+                             .replace('{{hpe}}', formatNumber(formData.hemoglobinaPromedio))
+                             .replace('{{cmh}}', formatNumber(formData.concentracionMediaHemoglobina))
+                             .replace('{{plaquetas}}', formatNumber(formData.plaquetas))
+                             .replace('{{leucocitos}}', formatNumber(formData.leucocitos))
+                             .replace('{{monocitos_rel}}', formatNumber(formData.monocitos))
+                             .replace('{{linfocitos_rel}}', formatNumber(formData.linfocitos))
+                             .replace('{{eosinofilos_rel}}', formatNumber(formData.eosinofilos))
+                             .replace('{{basofilos_rel}}', formatNumber(formData.basofilos))
+                             .replace('{{neutrofilos_segmentados_rel}}', formatNumber(formData.neutrofilosSegmentados))
+                             .replace('{{neutrofilos_banda_rel}}', formatNumber(formData.neutrofilosBanda))
+                             .replace('{{monocitos_abs}}', formatNumber(formData.monocitos))
+                             .replace('{{linfocitos_abs}}', formatNumber(formData.linfocitos))
+                             .replace('{{eosinofilos_abs}}', formatNumber(formData.eosinofilos))
+                             .replace('{{basofilos_abs}}', formatNumber(formData.basofilos))
+                             .replace('{{neutrofilos_segmentados_abs}}', formatNumber(formData.neutrofilosSegmentados))
+                             .replace('{{neutrofilos_banda_abs}}', formatNumber(formData.neutrofilosBanda));
 
     // Embed SVG images directly into the HTML
     htmlContent = htmlContent.replace('./img/top.svg', `data:image/svg+xml;base64,${Buffer.from(topImageContent).toString('base64')}`)
-                 .replace('./img/bottom.svg', `data:image/svg+xml;base64,${Buffer.from(bottomImageContent).toString('base64')}`);
+                             .replace('./img/bottom.svg', `data:image/svg+xml;base64,${Buffer.from(bottomImageContent).toString('base64')}`);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
--- tests/template.test.js
+++ tests/template.test.js
@@ -55,9 +55,39 @@ describe('PDF Generation', function() {
 
   it('should generate a PDF file', async function() {
     const formData = {
+      requerido: 'Veterinaria CaNinna',
+      nombrePropietario: 'Juan Pérez',
+      telefono: '1234567890',
       nombreMascota: 'Fido',
       especie: 'Perro',
-      raza: 'Labrador'
+      raza: 'Labrador',
+      edad: '5',
+      sexo: 'macho',
+      fecha: '2023-10-01',
+      eritrocitos: '7240000',
+      hemoglobina: '17.8',
+      hematocrito: '54.5',
+      volumenGlobularMedio: '75.2',
+      hemoglobinaPromedio: '24.6',
+      concentracionMediaHemoglobina: '32.7',
+      plaquetas: '339000',
+      leucocitos: '14400',
+      relativos: {
+        monocitos: '0',
+        linfocitos: '44',
+        eosinofilos: '18',
+        basofilos: '0',
+        neutrofilosSegmentados: '38',
+        neutrofilosBanda: '0'
+      },
+      absolutos: {
+        monocitos: '0',
+        linfocitos: '6336',
+        eosinofilos: '2592',
+        basofilos: '0',
+        neutrofilosSegmentados: '5472',
+        neutrofilosBanda: '0'
+      }
     };
 
     const outputDir = path.join(__dirname, 'test_output');
@@ -76,14 +106,57 @@ describe('PDF Generation', function() {
     // Load and read the HTML template
     let htmlContent = fs.readFileSync(templatePath, 'utf8');
 
+    // Format the date to dd/mm/yyyy
+    const [year, month, day] = formData.fecha.split('-');
+    const formattedDate = `${day}/${month}/${year}`;
+
+    // Format the age
+    const formattedAge = formData.edad === '1' ? '1 año' : `${formData.edad} años`;
+
+    function formatNumber(number) {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    function formatPhoneNumber(phoneNumber) {
+      const formatted = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '($1)$2-$3');
+      console.log('Formatted Phone Number:', formatted); // Debugging line
+      return formatted;
+    }
+
     // Replace placeholders with actual data
-    htmlContent = htmlContent.replace('{{nombreMascota}}', formData.nombreMascota)
+    htmlContent = htmlContent.replace('{{requerido}}', formData.requerido)
+                             .replace('{{fecha}}', formattedDate)
+                             .replace('{{nombreMascota}}', formData.nombreMascota)
                              .replace('{{especie}}', formData.especie)
-                             .replace('{{raza}}', formData.raza);
+                             .replace('{{raza}}', formData.raza)
+                             .replace('{{nombrePropietario}}', formData.nombrePropietario)
+                             .replace('{{edad}}', formattedAge)
+                             .replace('{{sexo}}', formData.sexo)
+                             .replace('{{telefono}}', formatPhoneNumber(formData.telefono))
+                             .replace('{{eritrocitos}}', formatNumber(formData.eritrocitos))
+                             .replace('{{hemoglobina}}', formatNumber(formData.hemoglobina))
+                             .replace('{{hematocrito}}', formatNumber(formData.hematocrito))
+                             .replace('{{vgm}}', formatNumber(formData.volumenGlobularMedio))
+                             .replace('{{hpe}}', formatNumber(formData.hemoglobinaPromedio))
+                             .replace('{{cmh}}', formatNumber(formData.concentracionMediaHemoglobina))
+                             .replace('{{plaquetas}}', formatNumber(formData.plaquetas))
+                             .replace('{{leucocitos}}', formatNumber(formData.leucocitos))
+                             .replace('{{monocitos_rel}}', formatNumber(formData.relativos.monocitos))
+                             .replace('{{linfocitos_rel}}', formatNumber(formData.relativos.linfocitos))
+                             .replace('{{eosinofilos_rel}}', formatNumber(formData.relativos.eosinofilos))
+                             .replace('{{basofilos_rel}}', formatNumber(formData.relativos.basofilos))
+                             .replace('{{neutrofilos_segmentados_rel}}', formatNumber(formData.relativos.neutrofilosSegmentados))
+                             .replace('{{neutrofilos_banda_rel}}', formatNumber(formData.relativos.neutrofilosBanda))
+                             .replace('{{monocitos_abs}}', formatNumber(formData.absolutos.monocitos))
+                             .replace('{{linfocitos_abs}}', formatNumber(formData.absolutos.linfocitos))
+                             .replace('{{eosinofilos_abs}}', formatNumber(formData.absolutos.eosinofilos))
+                             .replace('{{basofilos_abs}}', formatNumber(formData.absolutos.basofilos))
+                             .replace('{{neutrofilos_segmentados_abs}}', formatNumber(formData.absolutos.neutrofilosSegmentados))
+                             .replace('{{neutrofilos_banda_abs}}', formatNumber(formData.absolutos.neutrofilosBanda));
 
     // Embed SVG images directly into the HTML
     htmlContent = htmlContent.replace('./img/top.svg', `data:image/svg+xml;base64,${Buffer.from(topImageContent).toString('base64')}`)
-                 .replace('./img/bottom.svg', `data:image/svg+xml;base64,${Buffer.from(bottomImageContent).toString('base64')}`);
+                             .replace('./img/bottom.svg', `data:image/svg+xml;base64,${Buffer.from(bottomImageContent).toString('base64')}`);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
