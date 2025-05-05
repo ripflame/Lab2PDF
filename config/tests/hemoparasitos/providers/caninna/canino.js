6895d646e3bb0b5159e10a492b102f9e022edf22
Gilberto Leon <ripflame@gmail.com>
2025-05-04 22:27:06 -0600

    Adding hemoparasitos data file

---
+++ config/tests/hemoparasitos/providers/caninna/canino.js
@@ -0,0 +1,27 @@
+module.exports = {
+  id: "hemoparasitos_caninna_canino",
+  formFile: "hemoparasites.html",
+  templateFile: "hemoparasitesTemplate.html",
+  fields : [
+    {
+      id: "gusano",
+      label: "Gusano del Corazón",
+      type: "boolean",
+    },
+    {
+      id: "ehrlichiosis",
+      label: "Ehrlichiosis",
+      type: "boolean",
+    },
+    {
+      id: "lyme",
+      label: "Lyme",
+      type: "boolean",
+    },
+    {
+      id: "anaplasmosis",
+      label: "Anaplasmosis",
+      type: "boolean",
+    }
+  ]
+};
