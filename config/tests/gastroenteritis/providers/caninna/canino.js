4d859c9dfb7281301f979697095d1ede7684f45c
Gilberto Leon <ripflame@gmail.com>
2025-05-06 12:21:59 -0600

    Adding gastroenteritis test

---
+++ config/tests/gastroenteritis/providers/caninna/canino.js
@@ -0,0 +1,20 @@
+module.exports = {
+  id: "gastroenteritis",
+  formFile: "gastroenteritis.html",
+  templateFile: "gastroenteritisTemplate.html",
+  type: "testWithPhoto",
+  fields: [
+    {
+      id: "parvovirus",
+      label: "Parvovirus",
+    },
+    {
+      id: "coronavirus",
+      label: "Coronavirus",
+    },
+    {
+      id: "giardiasis",
+      label: "Giardiasi",
+    },
+  ]
+};
