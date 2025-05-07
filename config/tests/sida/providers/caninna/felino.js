936ada1e05a3696432e295b65ab340215c4af87d
Gilberto Leon <ripflame@gmail.com>
2025-05-06 18:58:24 -0600

    Adding sida felino test

---
+++ config/tests/sida/providers/caninna/felino.js
@@ -0,0 +1,21 @@
+module.exports = {
+  id: "sida",
+  formFile: "sida.html",
+  templateFile: "sidaTemplate.html",
+  type: "testWithPhoto",
+  fields: [
+    {
+      id: 'sida',
+      templateField: 'sida',
+      label: 'SIDA',
+    },
+    {
+      id: "leucemia",
+      label: "Leucemia",
+    },
+    {
+      id: "gusano",
+      label: "Gusano del Corazón",
+    },
+  ]
+};
