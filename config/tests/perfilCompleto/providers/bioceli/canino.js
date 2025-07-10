2f341fd25d9bd2399b7e429322316e4ffec6a6c5
Gilberto Leon <ripflame@gmail.com>
2025-07-09 19:31:49 -0600

    adding BIOCELI provider for 'Perfil Completo' and bumping to version 1.3.0 closes #10

---
+++ config/tests/perfilCompleto/providers/bioceli/canino.js
@@ -0,0 +1,176 @@
+module.exports = {
+  id: "perfilCompleto_bioceli",
+  formFile: "perfilCompleto_bioceli.html",
+  templateFile: "perfilCompletoTemplate_bioceli.html",
+  type: "table",
+  fields: [
+    {
+      id: 'alb',
+      templateField: 'alb',
+      label: 'Albumina (ALB)',
+      min: '2.6', 
+      max: '4.4', 
+      unit: 'g/dL'
+    },
+    {
+      id: 'tgo',
+      templateField: 'tgo',
+      label: 'TGO',
+      min: '12.0', 
+      max: '42.0', 
+      unit: 'g/dL'
+    },
+    {
+      id: 'tgp',
+      templateField: 'tgp',
+      label: 'TGP',
+      min: '15.0', 
+      max: '52.0', 
+      unit: 'U/L'
+    },
+    {
+      id: 'fosfatasa_alcalina',
+      templateField: 'fosfatasa_alcalina',
+      label: 'Fosfatasa Alcalina',
+      min: '23.0', 
+      max: '212.0', 
+      unit: 'U/L'
+    },
+    {
+      id: 'gamma_glutamil',
+      templateField: 'gamma_glutamil',
+      label: 'Gamma Glutamil',
+      min: '0.0', 
+      max: '12.0', 
+      unit: 'U/L'
+    },
+    {
+      id: 'proteinas_totales',
+      templateField: 'proteinas_totales',
+      label: 'Proteinas Totales',
+      min: '5.40', 
+      max: '7.20', 
+      unit: 'g/dL'
+    },
+    {
+      id: 'globulina',
+      templateField: 'globulina',
+      label: 'Globulina',
+      min: '2.70', 
+      max: '4.40', 
+      unit: 'g/dL'
+    },
+    {
+      id: 'rel_ag',
+      templateField: 'rel_ag',
+      label: 'Relacion A/G',
+      min: '0.6', 
+      max: '1.1', 
+      unit: ''
+    },
+    {
+      id: 'bilirrubina_total',
+      templateField: 'bilirrubina_total',
+      label: 'Bilirrubina Total',
+      min: '0.0', 
+      max: '0.80', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'bilirrubina_directa',
+      templateField: 'bilirrubina_directa',
+      label: 'Bilirrubina Directa',
+      min: '0.0', 
+      max: '0.25', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'bilirrubina_indirecta',
+      templateField: 'bilirrubina_indirecta',
+      label: 'Bilirrubina Indirecta',
+      min: '0.0', 
+      max: '0.40', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'dhl',
+      templateField: 'dhl',
+      label: 'Desihidrogensa Láctica (DHL)',
+      min: '', 
+      max: '', 
+      unit: ''
+    },
+    {
+      id: 'amilasa',
+      templateField: 'amilasa',
+      label: 'Amilasa',
+      min: '0.0', 
+      max: '1500.00', 
+      unit: 'u/L'
+    },
+    {
+      id: 'lipasa',
+      templateField: 'lipasa',
+      label: 'Lipasa',
+      min: '0.0', 
+      max: '240.00', 
+      unit: 'u/L'
+    },
+    {
+      id: 'glucosa',
+      templateField: 'glucosa',
+      label: 'Glucosa',
+      min: '70.0', 
+      max: '105.0', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'nitrogeno_ureico',
+      templateField: 'nitrogeno_ureico',
+      label: 'Nitrógeno Uréico',
+      min: '4,5', 
+      max: '30.5', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'creatinina',
+      templateField: 'creatinina',
+      label: 'Creatinina',
+      min: '0.6', 
+      max: '1.4', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'urea',
+      templateField: 'urea',
+      label: 'Urea',
+      min: '20.0', 
+      max: '50.0', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'acido_urico',
+      templateField: 'acido_urico',
+      label: 'Ácido Úrico',
+      min: '0.0', 
+      max: '2.0', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'colesterol',
+      templateField: 'colesterol',
+      label: 'Colesterol',
+      min: '135.0', 
+      max: '260.0', 
+      unit: 'mg/dL'
+    },
+    {
+      id: 'trigliceridos',
+      templateField: 'trigliceridos',
+      label: 'Trigliceridos',
+      min: '25.0', 
+      max: '120.0', 
+      unit: 'mg/dL'
+    },
+  ]
+};
